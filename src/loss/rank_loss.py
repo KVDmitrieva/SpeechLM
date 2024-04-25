c97b7deb903b64258e981115a8a91f61c41f6512
kvdmitrieva <katedv02@gmail.com>
2024-04-25 16:34:49 +0300

    small fix

--- src/loss/rank_loss.py
+++ src/loss/rank_loss.py
@@ -9,5 +9,5 @@ class RankLoss(nn.Module):
     def forward(self, fusion_score: torch.Tensor, l_value: torch.Tensor ,**batch):
         L_rank = - l_value * torch.log(fusion_score) - (1. - l_value) * torch.log(1. - fusion_score)
         return {
-            "loss": L_rank
+            "loss": L_rank.mean()
         }
