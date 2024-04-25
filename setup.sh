c2e357dc687128972ce3e9acdbd42039f79b7f95
kvdmitrieva <katedv02@gmail.com>
2024-04-25 23:02:02 +0300

    small changes

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
-mkdir data
-mkdir data/datasets
-mkdir data/datasets/asvspoof
+echo "Install requirements"
+pip install -r requirements.txt
 
-
-echo "Download dataset index"
-gdown "https://drive.google.com/u/0/uc?id=1bFEm0X8BF-X6l96Y__zV1Nxpc5sA2-_y" -O data/datasets/asvspoof/dev_index.json
-gdown "https://drive.google.com/u/0/uc?id=14Z0K8BqYXJzUBf2eK99t1apJKfYwp93Z" -O data/datasets/asvspoof/eval_index.json
-gdown "https://drive.google.com/u/0/uc?id=1IbHJs5Jj0cIioqGRVXCLwLzNrlvba1W-" -O data/datasets/asvspoof/train_index.json
+echo "Download SpeechLM checkpoint"
+pip install gdown
+gdown --fuzzy "https://drive.google.com/file/d/1azAQ_0AiwYSbmpwyTECCcraYKMkHBCxi/view?usp=sharing"
