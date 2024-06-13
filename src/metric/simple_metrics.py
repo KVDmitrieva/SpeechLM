1d00730d5bf7db0f39dccf5d2725bbda02546903
kvdmitrieva <katedv02@gmail.com>
2024-06-14 00:47:53 +0300

    change metrics

--- src/metric/simple_metrics.py
+++ src/metric/simple_metrics.py
@@ -7,9 +7,9 @@ class MeanCleanScore(BaseMetric):
     def __init__(self, name=None, *args, **kwargs):
         super().__init__(name, *args, **kwargs)
 
-    def __call__(self, x_prediction, y_prediction, l_value, **batch):
+    def __call__(self, x_prediction, y_prediction, l_value, fusion_score, **batch):
         mask = l_value > 0.5
-        mean_score = (x_prediction[mask].sum() + y_prediction[~mask].sum()) / l_value.shape[0]
+        mean_score = fusion_score[mask].mean()
         return mean_score
     
 
@@ -17,9 +17,9 @@ class MeanAugScore(BaseMetric):
     def __init__(self, name=None, *args, **kwargs):
         super().__init__(name, *args, **kwargs)
 
-    def __call__(self, x_prediction, y_prediction, l_value, **batch):
+    def __call__(self, x_prediction, y_prediction, l_value, fusion_score, **batch):
         mask = l_value < 0.5
-        mean_score = (x_prediction[mask].sum() + y_prediction[~mask].sum()) / l_value.shape[0]
+        mean_score = fusion_score[mask].mean()
         return mean_score
     
 
@@ -28,9 +28,9 @@ class Accuracy(BaseMetric):
         super().__init__(name, *args, **kwargs)
 
     def __call__(self, x_prediction, y_prediction, l_value, fusion_score, **batch):
-        preds = torch.cat([x_prediction.unsqueeze(0), y_prediction.unsqueeze(0)], dim=0)
-        labels = torch.min(preds, dim=0)[1]
-        true_labels = l_value > 0.5
+        # preds = torch.cat([x_prediction.unsqueeze(0), y_prediction.unsqueeze(0)], dim=0)
+        labels = fusion_score >= 0.5
+        true_labels = l_value >= 0.5
 
         mean_score = (true_labels == labels).float().mean()
         return mean_score
