cb5135987004dc7b24c47bd63ce14dcde86083c7
kvdmitrieva <katedv02@gmail.com>
2024-06-14 21:36:35 +0300

    add eps to log

--- src/loss/rank_loss.py
+++ src/loss/rank_loss.py
@@ -5,9 +5,10 @@ import torch.nn as nn
 class RankLoss(nn.Module):
     def __init__(self):
         super().__init__()
+        self.eps = 1e-9
 
     def forward(self, fusion_score: torch.Tensor, l_value: torch.Tensor ,**batch):
-        L_rank = - l_value * torch.log(fusion_score) - (1. - l_value) * torch.log(1. - fusion_score)
+        L_rank = - l_value * torch.log(fusion_score + self.eps) - (1. - l_value) * torch.log(1. - fusion_score + self.eps)
         return {
             "loss": L_rank.mean()
         }
